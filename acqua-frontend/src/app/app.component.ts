7187b792b68c13cfe134f2f6f9ec0672e04dbbb6
fernando <fernando@MacBook-Pro-de-fernando.local>
2023-06-05 17:51:30 -0600

    ng lint test

--- acqua-frontend/src/app/app.component.ts
+++ acqua-frontend/src/app/app.component.ts
@@ -8,6 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'acqua-frontend';
 
-  author = 'bitquark-systems';
+  author = "bitquark-systems";    
+
+
+  
 
 }
