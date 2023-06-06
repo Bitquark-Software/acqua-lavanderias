b92750f801f78195ab3aed13fb5b71211686b60b
fernando <fernando@MacBook-Pro-de-fernando.local>
2023-06-05 18:05:01 -0600

    lint autofix test

--- acqua-frontend/src/app/app.component.ts
+++ acqua-frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-})
+@Component ( {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+} )
 export class AppComponent {
-  title = 'acqua-frontend';
-
-  author = 'bitquark-systems';
-
+    title:string = 'acqua-frontend';
+    constructor(){
+        if( this.title == 'acqua' ){
+            this.title = 'app';
+        }
+    }
 }
