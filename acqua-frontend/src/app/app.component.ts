e7ffb64055bac66fff1d4671b63a98ef739ff74a
fernando <fernando@MacBook-Pro-de-fernando.local>
2023-06-05 18:06:38 -0600

    lint test

--- acqua-frontend/src/app/app.component.ts
+++ acqua-frontend/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 @Component ( {
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 } )
 export class AppComponent {
-    title = 'acqua-frontend';
-    constructor(){
-        if( this.title == 'acqua' ){
-            this.title = 'app';
-        }
+  title = 'acqua-frontend';
+  constructor(){
+    if( this.title == 'acqua' ){
+      this.title = 'app';
     }
+  }
 }
