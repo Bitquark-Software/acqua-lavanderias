10aef00c38ec457c83d09532100df5923f678c20
fernando <fernando@MacBook-Pro-de-fernando.local>
2023-06-07 22:04:22 -0600

    navbar y sidemenu creados

--- acqua-frontend/src/app/components/navbar/navbar.component.spec.ts
+++ acqua-frontend/src/app/components/navbar/navbar.component.spec.ts
@@ -14,4 +14,17 @@ describe ( 'NavbarComponent', () => {
   it ( 'should create', () => {
     expect ( component ).toBeTruthy ();
   } );
+  it ( 'should show all items when superAdmin is true', () => {
+    component.isAdmin = true;
+    fixture.detectChanges ();
+    const navbarLinks = fixture.nativeElement.querySelectorAll ('.drawer-side > .menu > li') as HTMLElement[];
+    expect ( navbarLinks.length ).toBe (5);
+  } );
+  it ( 'should show all items when superAdmin is false', () => {
+    component.isAdmin = false;
+    fixture.detectChanges ();
+    const navbarLinks = fixture.nativeElement.querySelectorAll ('.drawer-side > .menu > li') as HTMLElement[];
+    console.log (`length: ${navbarLinks.length}`);
+    expect ( navbarLinks.length ).toBe (2);
+  } );
 } );
--- acqua-frontend/tailwind.config.js
+++ acqua-frontend/tailwind.config.js
@@ -7,6 +7,23 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#113e47",
+          "secondary": "#a48976",
+          "accent": "#0b5456",
+          "neutral": "#2a323c",
+          "base-100": "#1d232a",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
 
