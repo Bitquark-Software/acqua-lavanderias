7fa58afffc760f559b8ff00c7d746a2a6914261a
ElianLvX <villagran-85-85@hotmail.com>
2023-06-10 20:12:42 -0600

    catalogo de servicios

--- acqua-backend/routes/api.php
+++ acqua-backend/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CatalogoController;
+use App\Http\Controllers\ServiciosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::apiResource('catalogos', CatalogoController::class); // CRUD CATALOGOS
+Route::apiResource('servicios', ServiciosController::class); // CRUD SERVICIOS
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
