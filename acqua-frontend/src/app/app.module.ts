6df2ba258c6db107131607a664d96954ccd41789
fernando <fernando@MacBook-Pro-de-fernando.local>
2023-06-15 07:12:24 -0600

    Pantallas de Cateogorias/Catalogo

--- acqua-frontend/src/app/app.module.ts
+++ acqua-frontend/src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { DrawerComponent } from './components/drawer/drawer.component';
+import { NuevoCatalogoComponent } from './components/catalogo/nuevo-catalogo/nuevo-catalogo.component';
+
+import { HotToastModule } from '@ngneat/hot-toast';
+
 @NgModule ( {
   declarations: [
     AppComponent,
     LoginComponent,
+    DrawerComponent,
     DashboardComponent,
+    CatalogoComponent,
+    NuevoCatalogoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    HotToastModule.forRoot({
+      dismissible: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
