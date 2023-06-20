e4ba5b95d01015ff05463c928693eaa82057667a
Debug <98186588+ElianLvX@users.noreply.github.com>
2023-06-19 21:21:06 -0600

    Agrega controlador de autenticación y migración de usuarios (#27)
    ### Autenticación y migración (Rol)
    
    - [x] Creacion de migracion para rol.
    - [x] Rutas con Passport.
    - [x] Controlador AuthController.
    - [x]  Modificación de AuthServiceProvider para Passport
    Y tambien se modificaron otros para el funcionamiento de passport

---
+++ acqua-backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => ['required','email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $accessToken = $user->createToken('login-token')->accessToken;
+
+            $userData = [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'role' => $user->role,
+            ];
+            
+            return new Response([
+                'access_token' => $accessToken,
+                'datos' => $userData,
+            ], 200);
+        }
+    
+        return new Response(['error' => 'Credenciales Incorrectas'], 401);
+    }
+
+    public function logout(Request $request): Response
+    {
+        $user = $request->user();
+
+        if ($user) {
+            $user->tokens()->delete(); // Revocar todos los tokens de acceso del usuario
+        }
+
+        return new Response(['message' => 'Sesion cerrada correctamente'], 200);
+    }
+}
--- acqua-backend/routes/api.php
+++ acqua-backend/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatalogoController;
 use App\Http\Controllers\ServiciosController;
-
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +23,10 @@ Route::apiResource('servicios', ServiciosController::class); // CRUD SERVICIOS
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('login', [AuthController::class, 'login'])
+    ->middleware(['throttle'])
+    ->name('login');
+
+Route::post('logout', [AuthController::class, 'logout'])
+    ->name('logout');
