c258b59d665d47f86e35df8ab9b7b19559e599ce
ElianLvX <villagran-85-85@hotmail.com>
2023-07-18 20:40:41 -0600

    Migracion, Model, Controller de Sucursal y Middleware a ruta Agregados

---
+++ acqua-backend/app/Http/Controllers/SucursalController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Sucursal;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class SucursalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Sucursal::paginate(5);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) : JsonResponse
+    {
+        $request->validate([
+            'nombre' => ['required','string', 'max:255']
+        ]);
+
+        $sucursal = Sucursal::create([
+            'nombre' => Str::upper($request->input('nombre'))
+        ]);
+
+        return response()->json([
+            'menssage' => 'Sucursal Creada Exitosamente',
+            'data' => $sucursal
+        ],201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Sucursal::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'nombre' => ['required','string', 'max:255']
+        ]);
+
+        $sucursal = Sucursal::findOrFail($id);
+        $sucursal->update([
+            'nombre' => Str::upper($request->nombre)
+        ]);
+
+        return response()->json([
+            'mensaje' => 'Sucursal Actualizada Correctamente...'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $sucursal = Sucursal::findOrFail($id);
+        $sucursal->delete();
+
+        return response()->json(['mensaje' => 'Sucursal Eliminada Correctamente'], 204);
+    }
+}
