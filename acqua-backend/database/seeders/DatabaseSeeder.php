6952740fa34c8cba0b31dd07d66cb0ba432b6193
fernando <fernando@MacBook-Pro-de-fernando.local>
2023-09-11 17:22:14 -0600

    seeders

--- acqua-backend/database/seeders/DatabaseSeeder.php
+++ acqua-backend/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            Sucursales::class,
+            ProcesoSeeder::class,
+            PrendasSeeder::class,
+            LavadorasSeeder::class,
+        ]);
     }
 }
