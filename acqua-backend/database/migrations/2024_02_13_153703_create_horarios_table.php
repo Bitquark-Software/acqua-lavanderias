4d8c49b947e38c3c1b4087e2dbf5308f48ef2877
ElianLvX <villagran-85-85@hotmail.com>
2024-02-15 17:15:58 -0600

    Controller, Model, nueva ruta en api.php y Migracion de Horarios

---
+++ acqua-backend/database/migrations/2024_02_13_153703_create_horarios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHorariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('horarios', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('sucursal_id');
+
+            $table->string('dias', 60);
+            $table->string('horario', 20);
+
+            $table->timestamps();
+
+            $table->foreign('sucursal_id')->references('id')->on('sucursales');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('horarios');
+    }
+}
