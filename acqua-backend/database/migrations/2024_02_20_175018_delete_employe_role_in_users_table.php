76951fb49921f759dc11a383746a87008e0b8fe5
ElianLvX <villagran-85-85@hotmail.com>
2024-02-24 01:30:32 -0600

    Migraciones para Agregar Roles de Encargado,Cajero y Opertativo tambien la actualizacion de Empleado a Cajero

---
+++ acqua-backend/database/migrations/2024_02_20_175018_delete_employe_role_in_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class DeleteEmployeRoleInUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            DB::statement("ALTER TABLE users MODIFY COLUMN role ENUM('administrador', 'encargado', 'operativo', 'cajero') DEFAULT 'operativo'");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            DB::statement("ALTER TABLE users MODIFY COLUMN role ENUM('administrador', 'empleado','encargado', 'operativo', 'cajero') DEFAULT 'operativo'");
+        });
+    }
+}
