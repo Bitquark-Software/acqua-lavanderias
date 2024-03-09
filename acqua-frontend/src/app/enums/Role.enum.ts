12f5e550e12954ec3f047008d6e25c7203973fce
Rodrigo <rodrigohip27989@disroot.org>
2024-03-09 12:52:18 -0600

    Refactorizacion de Role.enum.ts

--- acqua-frontend/src/app/enums/Role.enum.ts
+++ acqua-frontend/src/app/enums/Role.enum.ts
@@ -1,26 +1,8 @@
 /* eslint-disable no-unused-vars */
 export enum Role
 {
-    Administrador = 'administrador',
-    Encargado = 'encargado',
-    Operativo = 'operativo',
-    Cajero = 'cajero'
-}
-
-export function convertStringToRole(role = ''): Role | null
-{
-  const lowerCaseRole = role.toLowerCase();
-  switch(lowerCaseRole)
-  {
-  case 'administrador':
-    return Role.Administrador;
-  case 'encargado':
-    return Role.Encargado;
-  case 'operativo':
-    return Role.Operativo;
-  case 'cajero':
-    return Role.Cajero;
-  default:
-    return null;
-  }
+  Administrador = 'administrador',
+  Encargado = 'encargado',
+  Operativo = 'operativo',
+  Cajero = 'cajero'
 }
