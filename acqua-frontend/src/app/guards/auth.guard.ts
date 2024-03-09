13ad893f9a45961728bece70ff385b0abb24d50c
Rodrigo <rodrigohip27989@disroot.org>
2024-03-09 01:36:34 -0600

    Se agrego validacion en el guard referente al rol cajero y redirecciones

--- acqua-frontend/src/app/guards/auth.guard.ts
+++ acqua-frontend/src/app/guards/auth.guard.ts
@@ -1,12 +1,52 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { CanActivateFn, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../services/auth-service.service';
 import { Role } from '../enums/Role.enum';
 
 export const authGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
 ) =>
 {
+  const categoriesURLs: RegExp[] = [
+    /\/nueva-categoria$/,
+    /\/editar-categoria\/(\d+)$/,
+  ];
+
+  const servicesURLs: RegExp[] = [
+    /\/nuevo-servicio\/(\d+)$/,
+    /\/editar-servicio\/(\d+)$/,
+  ];
+
+  function checkURLBelongCategory(url: string): boolean
+  {
+    return categoriesURLs.some(route => route.test(url));
+  }
+
+  function checkURLBelongServices(url: string): boolean
+  {
+    return servicesURLs.some(route => route.test(url));
+  }
+
+  function getIdURLUsingRegExp(urlArray: RegExp[] = []): number | null
+  {
+    for (const regex of urlArray)
+    {
+      const match = state.url.match(regex);
+
+      if (match)
+      {
+        return Number(match[1]);
+      }
+    }
+    return null;
+  }
+
+  function getIdServiceURL()
+  {
+    return getIdURLUsingRegExp(servicesURLs);
+  }
+
   const session = inject(AuthService);
 
   if (!session.isLoggedIn || session.getUserRole() === null)
@@ -23,18 +63,34 @@ export const authGuard: CanActivateFn = (
       return true;
     }
     else
+    {
+      const router = inject(Router);
+
       if(session.getUserRole() === Role.Operativo)
       {
-        const router = inject(Router);
         router.navigate(['/tickets']);
         return true;
       }
-      else
+      if(session.getUserRole() === Role.Cajero && checkURLBelongCategory(state.url))
       {
-        const router = inject(Router);
-        router.navigate(['/dashboard']);
+        router.navigate(['/categorias']);
         return true;
       }
+      if(session.getUserRole() === Role.Cajero && checkURLBelongServices(state.url))
+      {
+        const id = getIdServiceURL();
+
+        if(id !== null)
+        {
+          router.navigate([`/ver-servicios/${id}`]);
+          return true;
+        }
+      }
+
+      router.navigate(['/dashboard']);
+      return true;
+    }
   }
+
   return session.isLoggedIn;
 };
