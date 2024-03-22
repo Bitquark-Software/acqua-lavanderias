b28b70dc7efeafc19ebfa1cc44bb70bed85021d5
rodrigo <rodrigohip27989@disroot.org>
2024-03-22 15:55:28 +0000

    Se creo el servicio 'admin-codes'

---
+++ acqua-frontend/src/app/services/admin-codes.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AdminCodesService } from './admin-codes.service';
+
+describe('AdminCodesService', () =>
+{
+  let service: AdminCodesService;
+
+  beforeEach(() =>
+  {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AdminCodesService);
+  });
+
+  it('should be created', () =>
+  {
+    expect(service).toBeTruthy();
+  });
+});
