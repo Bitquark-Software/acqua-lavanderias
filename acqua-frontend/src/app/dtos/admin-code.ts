5662c833f99b0a105092993d4b45685d952e2dd3
rodrigo <rodrigohip27989@disroot.org>
2024-03-22 16:12:49 +0000

    Se creo la clase 'AdminCode' y sus clases Response

---
+++ acqua-frontend/src/app/dtos/admin-code.ts
@@ -0,0 +1,43 @@
+
+export class AdminCode
+{
+  id?: number;
+  codigo?: string;
+  motivo?: string;
+  usado?: boolean;
+  id_ticket?: number;
+  id_user?: number;
+  used_at?: string;
+  created_at?: string;
+  updated_at?: string;
+}
+
+export class Link
+{
+  url?: string;
+  label?: string;
+  active?: boolean;
+}
+
+export class AdminCodeResponseGet
+{
+  current_page!: number;
+  data!: AdminCode[];
+  from?: number;
+  path?: string;
+  links?: Link[];
+  perl_page?: number;
+  first_page_url?: string;
+  last_page?: number;
+  prev_page_url ?: string;
+  next_page_url ?: string;
+  last_page_url?: string;
+  to?: number;
+  total?: number;
+}
+
+export class AdminCodeResponsePostPut
+{
+  data!: AdminCode;
+  mensaje!: string;
+}
