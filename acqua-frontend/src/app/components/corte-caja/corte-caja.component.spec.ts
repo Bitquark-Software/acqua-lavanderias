51a56263c0b70820d3660d3b640f395b7af49b75
rodrigo <rodrigohip27989@disroot.org>
2024-04-13 00:16:56 -0600

    Se creo el componente corte-caja

---
+++ acqua-frontend/src/app/components/corte-caja/corte-caja.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CorteCajaComponent } from './corte-caja.component';
+
+describe('CorteCajaComponent', () =>
+{
+  let component: CorteCajaComponent;
+  let fixture: ComponentFixture<CorteCajaComponent>;
+
+  beforeEach(() =>
+  {
+    TestBed.configureTestingModule({
+      declarations: [CorteCajaComponent],
+    });
+    fixture = TestBed.createComponent(CorteCajaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () =>
+  {
+    expect(component).toBeTruthy();
+  });
+});
