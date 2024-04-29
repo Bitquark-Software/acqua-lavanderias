eb2bf37e2d0103ef8d11d05c1e32fb21634f8b89
rodrigo <rodrigohip27989@disroot.org>
2024-04-28 22:02:17 -0600

    Se creo el componente 'modal-ganancias-caja' sin funcionalidad

---
+++ acqua-frontend/src/app/components/corte-caja/modal-ganancias-caja/modal-ganancias-caja.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-ganancias-caja',
+  templateUrl: './modal-ganancias-caja.component.html',
+  styleUrls: ['./modal-ganancias-caja.component.scss'],
+})
+export class ModalGananciasCajaComponent
+{
+
+}
