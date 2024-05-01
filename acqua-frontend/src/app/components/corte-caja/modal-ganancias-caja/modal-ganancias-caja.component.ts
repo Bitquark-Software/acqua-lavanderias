9dee9a7f3cfaa61dd104ada63b78072b54e6d121
rodrigo <rodrigohip27989@disroot.org>
2024-04-30 20:36:53 -0600

    Se agrego la funcionalidad del componente hijo 'modal-ganancias-caja' para reemplazar al modal correspondiente

--- acqua-frontend/src/app/components/corte-caja/modal-ganancias-caja/modal-ganancias-caja.component.ts
+++ acqua-frontend/src/app/components/corte-caja/modal-ganancias-caja/modal-ganancias-caja.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import {
+  GananciasResponseGet,
+  PROCESOS_CORTE_CAJA,
+  AnticiposEnvios,
+} from 'src/app/dtos/corte-caja';
 
 @Component({
   selector: 'app-modal-ganancias-caja',
   templateUrl: './modal-ganancias-caja.component.html',
   styleUrls: ['./modal-ganancias-caja.component.scss'],
 })
+
 export class ModalGananciasCajaComponent
 {
+  @Input() showModal = false;
+  @Input() current_process!: PROCESOS_CORTE_CAJA | null;
+  @Input() ganancias_caja!: GananciasResponseGet;
+  @Input() ganancias_caja_anticipos!: AnticiposEnvios;
 
+  closeModal(name_modal = '')
+  {
+    const modal = document.getElementById(name_modal);
+    if (modal instanceof HTMLDialogElement)
+    {
+      modal.close();
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges)
+  {
+    if (changes['current_process'] || changes['ganancias_caja'] || changes['ganancias_caja_anticipos'])
+    {
+      // console.log('Cambios en las propiedades de entrada');
+    }
+  }
 }
