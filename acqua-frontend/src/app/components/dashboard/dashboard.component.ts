39fa0d817a259e3456d983f9f765bc405d9cc416
rodrigo <rodrigohip27989@disroot.org>
2024-05-04 21:37:56 -0600

    Se creo la funcionalidad para cambiar la visibilidad de la caja segun su estado

--- acqua-frontend/src/app/components/dashboard/dashboard.component.ts
+++ acqua-frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { CajaStateService } from 'src/app/services/caja-state.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
 
+export class DashboardComponent
+{
+  mostrarCaja!: boolean;
+
+  constructor(private cajaStateService: CajaStateService)
+  {
+    //
+  }
+
+  ngOnInit()
+  {
+    this.cajaStateService.mostrarCajaEvent.subscribe((mostrar: boolean) =>
+    {
+      this.mostrarCaja = mostrar;
+    });
+  }
 }
